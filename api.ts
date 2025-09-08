1d435bcd41b7dc611b0a823cba3ebc863bbfb9b4
Melih <mlhtncc@gmail.com>
2025-09-08 15:24:20 +0300

    Remove events

--- api.ts
+++ api.ts
@@ -14,7 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 
 const KickBaseURL = "https://kick.com";
-const KickEventSubscription = "https://api.kick.com/public/v1/events/subscriptions";
 
 
 app.post('/get_stream_url', async (req, res) => {
@@ -34,109 +33,6 @@ app.post('/get_stream_url', async (req, res) => {
 		res.status(500).json({ error: err.stderr || err.message });
 	}
 });
-
-app.post('/get_subscribes', async (req, res) => {
-	const { accessToken } = req.body;
-	if (!accessToken) {
-		return res.status(400).json({ error: 'Access token is missing' });
-	}
-	
-	try {
-		const response = await fetch(KickEventSubscription, {
-			method: 'GET',
-			headers: {
-				"Authorization": `Bearer ${accessToken}`,
-				"Accept": "*/*"
-			},
-		});
-
-		if (!response.ok) {
-			throw new Error();
-		}
-
-		const data = await response.json();
-		return res.json(data);
-	} catch (err: any) {
-		res.status(500).json({ error: err.stderr || err.message });
-	}
-});
-
-app.post('/subscribe_chat', async (req, res) => {
-	const { userId, accessToken } = req.body;
-	if (!userId || !accessToken) {
-		return res.status(400).json({ error: 'User id or access token is missing' });
-	}
-	
-	const data = {
-		broadcaster_user_id: userId,
-		events: [
-			{
-				name: "chat.message.sent",
-				version: 1
-			}
-		],
-		method: "webhook"
-	};
-
-	try {
-		const response = await fetch(KickEventSubscription, {
-			method: 'POST',
-			headers: {
-				"Authorization": `Bearer ${accessToken}`,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(data),
-		});
-
-		if (!response.ok) {
-			throw new Error();
-		}
-
-		const responseJson = await response.json();
-		return res.json(responseJson);
-	} catch (err: any) {
-		res.status(500).json({ error: err.stderr || err.message });
-	}
-});
-
-app.post('/delete_subscribe', async (req, res) => {
-	const { id, accessToken } = req.body;
-	if (!id || !accessToken) {
-		return res.status(400).json({ error: 'Id or access token is missing' });
-	}
-
-	const params = new URLSearchParams();
-	params.append("id", id);
-
-	try {
-		const response = await fetch(`${KickEventSubscription}?${params.toString()}`, {
-			method: 'DELETE',
-			headers: {
-				"Authorization": `Bearer ${accessToken}`,
-				"Accept": "*/*"
-			},
-		});
-
-		if (response.status === 204) {
-			return res.json({ success: true });
-		} else {
-			return res.status(response.status).json({ success: false });
-		}
-
-	} catch (err: any) {
-		res.status(500).json({ error: err.stderr || err.message });
-	}
-});
-
-
-app.post('/kick_webhook', async (req, res) => {
-	
-	console.log(req.body);
-	
-
-	res.status(200).send();
-});
-
 
 
 const PORT = 5000;
